f2790835546c4bab4c5afe90c28b0d4bc7fd8e64
alopezbelles <alopezbelles@gmail.com>
2022-11-21 20:01:10 +0100

    creación REGISTER

---
+++ src/containers/Register/Register.jsx
@@ -0,0 +1,14 @@
+
+import React, { useState, useEffect } from "react";
+import "./Register.css";
+
+
+const Register = () => {
+
+    return <div className="registerDesign">Register</div>;
+
+    
+  }
+
+
+export default Register;
