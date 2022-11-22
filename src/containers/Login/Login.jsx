9b5d996837df8722bedd119e3a2ffc37c65b05dc
alopezbelles <alopezbelles@gmail.com>
2022-11-22 19:41:17 +0100

    diseño de vistas Login y Register

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -7,25 +7,30 @@ const Login = () => {
 
     return (
       
-      <div className="loginDesign"> Soy Login
+      <div className="container-fluid loginDesign d-flex justify-content-center align-items-center"> 
       
-      
-            <div className="inputsContainer">
-                <input type="text" name="email" placeholder= "Email ... |"  />
-                <input type="password" name="password" placeholder= "Password ... |" />
-                <Button variant="light">Light</Button>{' '}
+      <div className="row">
+          <div className="col inputsBox text-center align-items-center">
+
+            <p>LOG IN </p>
+
+            <input className="inputDesign" type="Email" placeholder="  Email ... |" />
+            <input className="inputDesign" type="Password" placeholder="  Password ... |" />
+            <input className="inputDesign" type="Password" placeholder="  Repeat Password ... |" />
+
+            <div className="col text-center align-items-center">
+
+            {/* Aquí quiero meter los botones, pero al importarlos de bootstrap me da un error */}
+            
             </div>
-      
+
+          </div>
+        </div>
+       
       
       </div>
 
     
-            // <div className="inputsContainer">
-            // <input type="text" name="email" placeholder= "Email ... |" onChange={(e) => inputHandler(e)} />
-            // <input type="password" name="password" placeholder= "Password ... |" onChange={(e) => inputHandler(e)} />
-            // <Button variant="light">Light</Button>{' '}
-            // </div>
-
       
 
       )    
--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -1,51 +1,36 @@
 
 import React, { useState, useEffect } from "react";
 import "./Register.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 const Register = () => {
 
   return (
 
-    <div className=" container-fluid registerDesign">
+    <div className="container-fluid registerDesign  d-flex justify-content-center align-items-center">
 
-      <div className=" container inputsBox"> 
-
-        <div className="row" >
-
-          <div className="col">
+        <div className="row">
+          <div className="col inputsBox text-center align-items-center">
 
             <p>REGISTER </p>
 
-            <input className="inputDesign" type="Email" placeholder="Email ... |" />
-            <input className="inputDesign" type="Password" placeholder="Email ... |" />
+            <input className="inputDesign" type="Name" placeholder="  User name ... |" />
+            <input className="inputDesign" type="Email" placeholder="  Email ... |" />
+            <input className="inputDesign" type="Password" placeholder="  Password ... |" />
+            <input className="inputDesign" type="Password" placeholder="  Repeat password ... |" />
+
+            <div className="col text-center align-items-center">
+
+            {/* Aquí quiero meter los botones, pero al importarlos de bootstrap me da un error */}
+            </div>
+
 
 
 
           </div>
-
-
-
-
         </div>
-
-
-      </div>
-
-
-
-      {/* <input type="text" name="email" placeholder= "Email ... |"  />
-                <input type="password" name="password" placeholder= "Password ... |" /> */}
-
-
-
-
-
     </div>
-
-
-
-
 
   )
 }
