57d1692f88dc15b494361134b6e911a8ff0d6354
alopezbelles <alopezbelles@gmail.com>
2022-11-23 15:57:46 +0100

    funcionalidad register

--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -5,41 +5,144 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Image from 'react-bootstrap/Image' //esto lo añado para poder importar una imagen
 import boca from '../../images/boca.png'
 import rayo from '../../images/rayo.png'
-
+import { userData } from "../User/userSlice";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { errorCheck } from "../../services/usefull";
+import axios from "axios"
 
 const Register = () => {
 
+  //Nos traemos las credenciales
+  const credentials = useSelector(userData);
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState({
+
+    username: "",
+    name: "",
+    address: "",
+    city: "",
+    email: "",
+    password: "",
+    password2: "",
+
+  })
+
+  const [userError, setUserError] = useState({
+
+    usernameerror: "",
+    nameerror: "",
+    addresserror: "",
+    cityerror: "",
+    emailerror: "",
+    passworderror: "",
+    password2error: "",
+    incompleteerror: "",
+  })
+
+  const registerUser = async (body) => {
+    let res = await axios.post("http://localhost:33061/user/register", body)
+  }
+
+  //Handlers//
+
+  const inputHandler = (e) => {
+    setUser((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+
+    }))
+  }
+
+  const errorHandler = (field, value, type, password1) => {
+
+    let error = "";
+    error = errorCheck(value, type, password1)
+    setUserError((prevState) => ({
+      ...prevState,
+      [field + "error"]: error
+    }))
+
+  }
+
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (validateBody(body)) {
+      setUserError((prevState) => ({
+        ...prevState,
+        incompleteerror: "",
+
+      }))
+      registerUser(body)
+    }
+    else {
+      setUserError((prevState) => ({
+        ...prevState,
+        incompleteerror: "You must complete every field",
+    }))
+  }}
+
+
+  const body = {
+    username: user.username,
+    name: user.name,
+    address: user.address,
+    city: user.city,
+    email: user.email,
+    password: user.password,
+    password2: user.password2,
+  }
+
+
+  const validateBody = (body) => {
+    if (body.username !== "" && body.name !== "" && body.address !== "" && body.city !== "" && body.email !== "" && body.password !== "" && body.password2 !== "") { return true }
+
+  }
+
   return (
 
-    <div className="container-fluid registerDesign d-flex justify-content-center align-items-center">
+    <form onSubmit={submitHandler} className="container-fluid registerDesign d-flex justify-content-center align-items-center">
 
       <Image className="bocaImage zoom" src={boca}></Image>
       <Image className="rayoImage zoom" src={rayo}></Image>
 
 
-        <div className="row">
-          <div className="col-12 inputsBox text-center align-items-center">
+      <div className="row">
+        <div className="col-12 inputsBox text-center align-items-center">
+          <div>{userError.incompleteerror}</div>
 
-            <p>REGISTER </p>
+          <p>REGISTER </p>
 
-            <input className="inputDesign" type="Name" placeholder="  User name ... |" />
-            <input className="inputDesign" type="Email" placeholder="  Email ... |" />
-            <input className="inputDesign" type="Password" placeholder="  Password ... |" />
-            <input className="inputDesign" type="Password" placeholder="  Repeat password ... |" />
+          <input onBlur={(e) => errorHandler(e.target.name, e.target.value, "text")} onChange={inputHandler} className="inputDesign" type="text" placeholder="  User name ... |" name="username" />
+          <div className="errorInput">{userError.usernameerror}</div>
+          <input onBlur={(e) => errorHandler(e.target.name, e.target.value, "text")} onChange={inputHandler} className="inputDesign" type="text" placeholder="  Name ... |" name="name" />
+          <div className="errorInput">{userError.nameerror}</div>
+          <input onBlur={(e) => errorHandler(e.target.name, e.target.value, "text")} onChange={inputHandler} className="inputDesign" type="text" placeholder="  Address ... |" name="address" />
+          <div className="errorInput">{userError.addresserror}</div>
+          <input onBlur={(e) => errorHandler(e.target.name, e.target.value, "text")} onChange={inputHandler} className="inputDesign" type="text" placeholder="  City ... |" name="city" />
+          <div className="errorInput">{userError.cityerror}</div>
+          <input onBlur={(e) => errorHandler(e.target.name, e.target.value, "email")} onChange={inputHandler} className="inputDesign" type="Email" placeholder="  Email ... | " name="email" />
+          <div className="errorInput">{userError.emailerror}</div>
+          <input onBlur={(e) => errorHandler(e.target.name, e.target.value, "password")} onChange={inputHandler} className="inputDesign" type="Password" placeholder="  Password ... |" name="password" />
+          <div className="errorInput">{userError.passworderror}</div>
+          <input onBlur={(e) => errorHandler(e.target.name, e.target.value, "password2", user.password)} onChange={inputHandler} className="inputDesign" type="Password" placeholder="  Repeat password ... |" name="password2" />
+          <div className="errorInput">{userError.password2error}</div>
 
-            <div className="col text-center align-items-center">
+          <div className="col text-center align-items-center">
 
-              <button className="buttonDesign">Register</button>
-              
-            
-            </div>
-
-
+            <button className="buttonDesign">Register</button>
 
 
           </div>
+
+
+
+
         </div>
-    </div>
+      </div>
+    </form>
 
   )
 }
