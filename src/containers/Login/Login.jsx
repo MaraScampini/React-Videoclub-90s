c724e6dbc1c6a8c28dd319760d4f7b1dd5b5bd20
alopezbelles <alopezbelles@gmail.com>
2022-11-24 18:26:25 +0100

    estilos login y register

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -7,36 +7,58 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const Login = () => {
 
-    return (
-      
-      <div className="container-fluid loginDesign d-flex justify-content-center align-items-center"> 
-      
-      <div className="row">
-          <div className="col inputsBox text-center align-items-center">
+  return (
 
-            <p>LOG IN </p>
+    <form
+      className="container-fluid loginDesign d-flex justify-content-center align-items-center"
+    >
+
+      <div className="row">
+        <div className="col inputsBox text-center align-items-center">
+
+          <div className="row">
+          <div className="col text-center align-items-center" id="inputsBox">
+
+          <h3>LOG IN </h3>
+          <hr id="line"></hr>
 
             <input className="inputDesign" type="Email" placeholder="  Email ... |" />
             <input className="inputDesign" type="Password" placeholder="  Password ... |" />
-            
 
-            <div className="col text-center align-items-center">
 
-            <button className="buttonDesign">Log In</button>
+<div className="col text-center align-items-center">
 
-            </div>
+  <button className="buttonDesign">Log In</button>
+
 
           </div>
+
+
+
+          </div>
+
+         
+
+          </div>
+
         </div>
-       
-      
       </div>
 
-    
-      
 
-      )    
-  }
+    </form>
+
+    // <div className="container-fluid loginDesign d-flex justify-content-center align-items-center"> 
+
+
+
+
+    // </div>
+
+
+
+
+  )
+}
 
 
 export default Login;
