25c3a2190c1242b7c244083a9502593d0b5817af
MaraScampini <109754827+MaraScampini@users.noreply.github.com>
2022-12-02 12:18:36 +0100

    Added extensions

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import filmSlice from "../components/Films/filmSlice";
+import filmSlice from "../components/Films/filmSlice.js";
 
 export default configureStore({
   reducer: {
