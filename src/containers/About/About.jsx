805ba90a98a4c09aa1dee5dda263da57ade44390
MaraScampini <109754827+MaraScampini@users.noreply.github.com>
2022-12-04 13:32:25 +0100

    Fix About Us page and translate

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -10,14 +10,16 @@ import logo from "../../assets/logo.png";
 const About = () => {
 
     return (
-        <Container className="container-fluid aboutDesign">
+        <Container fluid className="aboutDesign">
             <Row className="row rowDesign">
                 <Col className="col-12 col-md-6 col1 leftTarget d-flex justify-content-center align-items-center">
 
                     <div className="aboutTarget text-center">
 
                         <h3 className="tittleAbout">VUELVE A LOS 90s </h3>
-                        <p className="p-3">Las mejores películas de los años noventa demuestran que, sin ellas, sería imposible entender la historia de Hollywood, y del cine mundial. En aquellos maravillosos 90 muchos encontramos nuestras películas favoritas, y los mejores carteles de películas para decorar las paredes de nuestros dormitorios. De las mejores películas de terror de la historia del cine hasta los mejores musicales o las mejores películas animadas de Disney, SUPER EIGHT te ayuda a celebrar el increíble legado que nos dejó esta gran década. Hemos recopilado los mejores títulos de una época única, marcada por la ropa hortera, los Tamagochi, 'Salvados por la campana' y también, como veremos, el mejor cine.</p>
+                        <p className="p-3">
+                            The best movies from the 90s show that, without them, it would be impossible to understand Hollywood's, and cinema as a whole, history. The wonderful 90s gave us many of our favourite movies and the best movie posters, that would end up decorating every teenage room in the world. <br/>From the best horror movies to the greatest musicals and the funniest Disney movies, SUPER EIGHT will help you celebrate the legacy of the most amazing decade. We've curated a list with the best of the best from a unique era, that shone in tacky clothing, Tamagotchi, <i>Saved by the Bell</i> and, as you can see, the best movies.
+                            </p>
 
                     </div>
 
@@ -29,8 +31,12 @@ const About = () => {
                     <div className="superEightTarget text-center">
 
                         <Image className="logoImageAbaut" src={logo}></Image>
-                        <p className="p-3"> Desde 'Pulp Fiction' hasta 'El show de Truman', desde 'Todo sobre mi madre' hasta 'Titanic', seleccionamos las películas que marcaron la década de los noventa. ¿Cuál es tu favorita? </p>
-                        <p className="p-3"> Si has llegado hasta quí es porque SUPER EIGHT es un videoclub online hecho a tu medida. Si creciste con las mejores películas de los 90, te ofrecemos la oportunidad de rememorar algunos de los mejores clásicos desde el sofá de tu casa. ¡Dale al play y disfruta! </p>
+                        <p className="p-3"> 
+                        From <i>Pulp Fiction</i> to <i>Truman's Show</i>, <i>Toy Story</i> to <i>Titanic</i>, we've curated for you the best 90s movies, those that left their mark on a generation. Which is your favourite?
+                        </p>
+                        <p className="p-3"> 
+                        If you're here, SUPER EIGHT is a videoclub catered exclusively for you. Either if you grew up watching the best movies from the 90s or want to get into that amazing era, we offer the opportunity to remember some of the best classics from then without leaving your couch. Hit play and enjoy!
+                        </p>
                     </div>
 
                 </Col>
